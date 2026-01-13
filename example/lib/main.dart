ab6db4feaa68f273ca8806bc3d85768490baf56f
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2026-01-13 09:17:15 +0630

    Fixed timezone offset calculation

---
+++ example/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:myanmar_calendar_dart/myanmar_calendar_dart.dart';
+
+void main() {
+  // Configure the calendar (optional)
+  MyanmarCalendar.configure(
+    language: Language.english,
+    timezoneOffset: 0, // Myanmar Standard Time
+  );
+
+  // Get today's date
+  final today = MyanmarCalendar.today();
+  print('Today: ${today.formatComplete()}');
+
+  // Convert Western date to Myanmar
+  final myanmarDate = MyanmarCalendar.fromWestern(1998, 7, 27);
+  print('Myanmar: ${myanmarDate.formatMyanmar()}');
+  print('Western: ${myanmarDate.formatWestern()}');
+}
